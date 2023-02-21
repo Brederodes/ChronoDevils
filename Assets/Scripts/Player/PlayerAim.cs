3c481c7c753dc3ff5ed568006ba935670426db6c
brederodes <anacleciobrederodes@outlook.com>
2023-02-21 07:31:47 -0300

    quick round mechanic, planning to add the timetravel mechanic later today.

--- Assets/Scripts/Player/PlayerAim.cs
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class PlayerAim : MonoBehaviour
 {
-    [SerializeField] private bool isAiming;
+    [SerializeField] public bool isAiming {get; private set;}
+    [SerializeField] public float aimLineLength= 5f;
+    public Vector3 actualAimDirection {get; private set;}
     Ray cameraRay;
     RaycastHit cameraHit;
+    LineRenderer aimRay;
     Vector3 cameraXZHitPoint;
-    Vector3 aimDirection;
+    Vector3 desiredAimDirection;
     Quaternion lookAtQuaternion;
     [SerializeField] float aimSmoothness = 15f;
 
-    // Update is called once per frame
-    void Update(){
+    void Start(){
+        aimRay= gameObject.GetComponent<LineRenderer>();
+    }
+
+    void LateUpdate(){
         if(Input.GetMouseButton(1)){
             cameraXZHitPoint= getXZHitPoint();
             if(cameraXZHitPoint.y != 0){
@@ -21,10 +27,19 @@ public class PlayerAim : MonoBehaviour
                 return;
             }
             isAiming= true;
-            aimDirection= cameraXZHitPoint - new Vector3(transform.position.x, 0, transform.position.z);
-            lookAtQuaternion= Quaternion.LookRotation(aimDirection, Vector3.up);
+
+            desiredAimDirection= cameraXZHitPoint - new Vector3(transform.position.x, 0, transform.position.z);
+            lookAtQuaternion= Quaternion.LookRotation(desiredAimDirection, Vector3.up);
             transform.rotation = Quaternion.Slerp(transform.rotation, lookAtQuaternion, Time.deltaTime * aimSmoothness);
+            
+            actualAimDirection= transform.forward;
+
+            aimRay.enabled= true;
+            aimRay.SetPosition(0, transform.position);
+            aimRay.SetPosition(1, transform.position+transform.forward*aimLineLength);
+            
         } else {
+            aimRay.enabled= false;
             isAiming= false;
         }
     }
